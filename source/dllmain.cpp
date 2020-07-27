72a88677eb810289b4a443e0eb462574b2ae6ba0
ermaccer <40604575+ermaccer@users.noreply.github.com>
2020-07-27 15:05:01 +0200

    Add files via upload

--- source/dllmain.cpp
+++ source/dllmain.cpp
@@ -4,28 +4,42 @@
 
 using namespace Memory::VP;
 
+DWORD GetFilePointer(HANDLE hFile)
+{
+	return SetFilePointer(hFile, 0, NULL, FILE_CURRENT);
+}
+
+
+
 
 BOOL __stdcall ReadFile_hook(HANDLE hFile, LPVOID lpBuffer, DWORD nNumberOfBytesToRead, LPDWORD lpNumberOfBytesRead, LPOVERLAPPED lpOverlapped) {
 
 	BOOL result = ReadFile(hFile, lpBuffer, nNumberOfBytesToRead, lpNumberOfBytesRead, lpOverlapped);
 
-	if (lpOverlapped->Offset >= GetFileSize(hFile,NULL))
-		SetLastError(ERROR_HANDLE_EOF);
+	if (lpOverlapped)
+	{
+		if (lpOverlapped->Offset >= GetFileSize(hFile, NULL))
+			SetLastError(ERROR_HANDLE_EOF);
+		else
+			SetLastError(ERROR_IO_PENDING);
+	}
 	else
-     	SetLastError(ERROR_IO_PENDING);
+	{
+		if (GetFilePointer(hFile) >= GetFileSize(hFile, NULL))
+			SetLastError(ERROR_HANDLE_EOF);
+		else
+			SetLastError(ERROR_IO_PENDING); // not needed?
+	}
 
 	return result;
 }
 
 
 
+
 void Init()
 {
-
-	Nop(0x45A24B, 6);
-	InjectHook(0x45A24B, ReadFile_hook, PATCH_CALL);
-	Nop(0x45A2FE, 6);
-	InjectHook(0x45A2FE, ReadFile_hook, PATCH_CALL);
+	Patch<int>(0x81B374, (int)ReadFile_hook);
 }
 
 BOOL APIENTRY DllMain( HMODULE hModule,
