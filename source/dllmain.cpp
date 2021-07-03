838285f12488bc73b87f64896f08be7a2840dcae
Fire-Head <Fire-Head@users.noreply.github.com>
2021-07-03 04:01:55 +0300

    fix movies

--- source/dllmain.cpp
+++ source/dllmain.cpp
@@ -34,12 +34,66 @@ BOOL __stdcall ReadFile_hook(HANDLE hFile, LPVOID lpBuffer, DWORD nNumberOfBytes
 	return result;
 }
 
+void __declspec(naked) Movies_hook()
+{
+	__asm
+	{		
+		mov     eax, dword ptr ds: [7C9970h] // 7C9970h
+		cmp     eax, 0
+		jnz     SECONDPART
+		
+		mov     eax, dword ptr ds: [736DB8h] // Skel
+		mov     edx, eax
+		mov     eax, edx
+		mov     ecx, [eax+0Ch]
+		mov     ebx, [ecx]
+		call    dword ptr [ebx+2Ch]
+		
+		mov     eax, dword ptr ds: [736DB8h] // Skel
+		mov     edx, eax
+		mov     eax, edx
+		mov     ecx, [eax+0Ch]
+		mov     ebx, [ecx]
+		call    dword ptr [ebx+28h]
+		
+		mov     ecx, 725A4Ch // g_TextLevel
+		mov     eax, 4932F0h
+		call    eax          // CText::LoadLevel
+		
+		mov     dword ptr ds: [7C9970h], 1
+		
+		mov     eax, 4C3650h
+		call    eax          // CApp::PlayBinkMovie
+		
+		mov     eax, 4D7E30h
+		jmp     eax
+		
+SECONDPART:
+		mov     ecx, 7A9EBCh // MusicManager
+		mov     eax, 5A5A50h
+		call    eax          // cMusicManager::PlayLevelCompletedMusic
+		
+		
+		push    2
+		mov     eax, 5DBDC0h
+		call    eax          // CGameInfo::Show
+		pop     ecx
+		
+		mov     dword ptr ds: [7C86E0h], 0 // CFrontendMenu::m_bOkToLoadNextLevel
+	
+		mov     dword ptr ds: [755E4Ch], 6 // CApp::ms_mode
+		
+		mov     eax, 4D7E26h
+		jmp     eax
+	}
+}
 
 
 
 void Init()
 {
 	Patch<int>(0x81B374, (int)ReadFile_hook);
+	Patch<int>(0x673F30, (int)Movies_hook);
 }
 
 BOOL APIENTRY DllMain( HMODULE hModule,
